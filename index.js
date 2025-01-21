9b84de6054975777c1f3ce8044470e6995db463e
Pulves <oracioc@alu.ufc.br>
2025-01-20 21:37:11 -0300

    created 'read' and 'create' functionalities for custumer object

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express');
 const axios = require('axios');
 const app = express();
+
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
@@ -8,19 +10,126 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.TOKEN;
+
+const rootAPI = "https://api.hubapi.com";
+
+const headers = {
+    "Authorization": `Bearer ${PRIVATE_APP_ACCESS}`,
+    "Content-Type": "application/json"
+};
+
+const instance = axios.create({
+    baseURL: rootAPI,
+    headers: headers
+});
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get("/", async (request, response) => {
+    const name = 'frutas';
+    const objectType = "2-39592045";
+    const objectID = "22668265238"
+    const data = {
+        "properties": ["name", "amount", "quantity", "total_amount"],
+       
+    }
+
+    const listInput = [];
+
+    const customObjURL = `/crm/v3/objects/${name}/batch/read`;
+    const getCustomObjURL = `/crm/v3/objects/${name}`;
+    const parameters = {
+        limit: 10,
+        after: undefined,
+        properties: ["name", "amount", "quantity", "total_amount"],
+        propertiesWithHistory: undefined,
+        associations: undefined,
+        archived: false
+    };
+
+    //pega os ids do objeto personalizado
+    await instance.get(getCustomObjURL, {parameters}).then(resp => {
+        
+        for (const key in resp.data.results){
+        listInput.push({'id': resp.data.results[key].id});
+    };
+
+    }).catch(erro => {
+        console.error(erro);
+    });
+
+    //insere a lista de ids no atributo input do objeto data
+    data['inputs'] = listInput;
+
+    //faz a requizição para coletar um conjunto de records do objeto personalizado
+    //retorna os dados de cada record como uma lista de objetos
+    const respAPI = await instance.post(customObjURL, data).then(resp => {
+        
+        let customObject = [];
+        let results = resp.data.results;
+
+        for (const key in results){
+            customObject.push(    
+                {
+                "name": results[key].properties.name,
+                "quantity": results[key].properties.quantity,  
+                "amount": results[key].properties.amount,  
+                "total_amount": results[key].properties.total_amount,  
+
+                }
+            );
+        };
+        return customObject;
+
+    }).catch(error => {
+        console.error(error);
+    });
+
+
+    
+    // response.send(respAPI)
+    response.render('homepage.pug', 
+        {
+            customObjects: respAPI, 
+            title: "Custom Object Table"
+        }
+    )
+});
+
+
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
+app.get('/update-cobj', (request, response) => {
+    response.render('update.pug', {
+        title: "Update Custom Object Form | Integrating With HubSpot I Practicum"
+    });
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-// * Code for Route 3 goes here
+app.post('/update-cobj', async (request, response) => {
+   
+    const nameObject ="frutas";
+    const {name, amount, quantity} = request.body;
+    const updateCustomURL = `/crm/v3/objects/${nameObject}`;
+    
+    const properties = {
+        'name': name,
+        'amount': amount,
+        'quantity': quantity
+    };
+
+    const responseAPI = await instance.post(updateCustomURL, {properties}).then(resp => {
+        return resp.status;
+    }).catch(error => {
+        console.error(error);
+    });
+    
+    console.log("response API: ", responseAPI);
+    response.redirect('/')
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
