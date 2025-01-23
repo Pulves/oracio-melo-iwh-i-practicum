79ad4405ed91f67f99871a111a3362ab8ff30de0
Pulves <oracioc@alu.ufc.br>
2025-01-23 13:55:54 -0300

    add button to form in homepage and improvement button events

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,12 +1,27 @@
 const buttonElement = document.getElementsByClassName("butom-options")[0];
 const selectElement = document.getElementsByClassName("button-update-choice")[0];
+const cancelBntElement = document.getElementsByClassName("button-cancel")[0];
 
+function closeSelectForm(){
 
-function openOptions(){
-    document.getElementById("myButton").style.display = "flex";
+    buttonElement.removeEventListener("click", openSelectform, false);
+    document.getElementById("selectObject").style.display = "none";
+    buttonElement.addEventListener("click", openOptions, false);
 }
 
-function openSelectform(){
+function openOptions(){
+    if(document.getElementById("myButton").style.display != "flex"){
+        document.getElementById("myButton").style.display = "flex";
+    }
+
+    else{
+
+        document.getElementById("myButton").style.display = "none";
+        
+    }
+}
+
+function openSelectform(){ 
     document.getElementById("selectObject").style.display = "flex";
     buttonElement.removeEventListener("click", openOptions, false);
     document.getElementById("myButton").style.display = "none";
@@ -15,8 +30,8 @@ function openSelectform(){
 
 document.addEventListener('DOMContentLoaded', function () {
     buttonElement.addEventListener("click", openOptions, false);
-
     selectElement.addEventListener('click', openSelectform, false);
+    cancelBntElement.addEventListener('click', closeSelectForm, false);
 });
 
 
