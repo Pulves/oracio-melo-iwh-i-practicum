b932664abf189b72bfff19ca5ebeaeba2df25fc3
Pulves <oracioc@alu.ufc.br>
2025-01-22 21:11:08 -0300

    project demo workable

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ require('dotenv').config()
 const express = require('express');
 const axios = require('axios');
 const app = express();
-
+const fs = require('fs');
+const { error } = require('console');
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
@@ -11,6 +12,7 @@ app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
 const PRIVATE_APP_ACCESS = process.env.TOKEN;
+const OBJECT_TYPE = process.env.OBJECT_TYPE
 
 const rootAPI = "https://api.hubapi.com";
 
@@ -25,11 +27,11 @@ const instance = axios.create({
 });
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+//PRIMEIRO FOI COLETADO OS ID DOS RECORDS E COM BASE NESSES ID FOI FEITO OUTRA CONSULTA
+//NA API PARA COLETAR O CONTEUDO DOS RECORDS
 
 app.get("/", async (request, response) => {
-    const name = 'frutas';
-    const objectType = "2-39592045";
-    const objectID = "22668265238"
+    
     const data = {
         "properties": ["name", "amount", "quantity", "total_amount"],
        
@@ -37,8 +39,8 @@ app.get("/", async (request, response) => {
 
     const listInput = [];
 
-    const customObjURL = `/crm/v3/objects/${name}/batch/read`;
-    const getCustomObjURL = `/crm/v3/objects/${name}`;
+    const customObjURL = `/crm/v3/objects/${OBJECT_TYPE}/batch/read`;
+    const getCustomObjURL = `/crm/v3/objects/${OBJECT_TYPE}`;
     const parameters = {
         limit: 10,
         after: undefined,
@@ -51,9 +53,12 @@ app.get("/", async (request, response) => {
     //pega os ids do objeto personalizado
     await instance.get(getCustomObjURL, {parameters}).then(resp => {
         
+        //percorre a lista de objetos results pegando seu indice
+        //salva o id de cada record em uma lista
+        //console.log(`Results id: ${JSON.stringify(resp.data.results)}`); 
         for (const key in resp.data.results){
         listInput.push({'id': resp.data.results[key].id});
-    };
+        };
 
     }).catch(erro => {
         console.error(erro);
@@ -68,6 +73,7 @@ app.get("/", async (request, response) => {
         
         let customObject = [];
         let results = resp.data.results;
+        console.log(`records results: ${JSON.stringify(results)}`)
 
         for (const key in results){
             customObject.push(    
@@ -76,10 +82,11 @@ app.get("/", async (request, response) => {
                 "quantity": results[key].properties.quantity,  
                 "amount": results[key].properties.amount,  
                 "total_amount": results[key].properties.total_amount,  
-
+                "id": results[key].id
                 }
             );
         };
+        
         return customObject;
 
     }).catch(error => {
@@ -87,7 +94,7 @@ app.get("/", async (request, response) => {
     });
 
 
-    
+    fs.writeFileSync("./stored.json", JSON.stringify(insertID(respAPI)), (error) => error && console.error(error))
     // response.send(respAPI)
     response.render('homepage.pug', 
         {
@@ -101,33 +108,59 @@ app.get("/", async (request, response) => {
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-app.get('/update-cobj', (request, response) => {
-    response.render('update.pug', {
-        title: "Update Custom Object Form | Integrating With HubSpot I Practicum"
-    });
+app.get('/update-cobj/', (request, response) => {
+    let objectToUpdate = undefined;
+    if (request.query.id){
+        let fileContent = fs.readFileSync('./stored.json', 'utf-8',(error) => error && console.error(error));
+        let newObject = JSON.parse(fileContent);
+        newObject.forEach(object => {
+            if (object.id = request.query.id){
+                objectToUpdate = object;
+            }
+        });
+        response.render('update.pug', {
+            title: "Update Custom Object Form | Integrating With HubSpot I Practicum",
+            customObject: objectToUpdate
+        });
+    }
+    else{
+        response.render('update.pug', {
+            title: "Update Custom Object Form | Integrating With HubSpot I Practicum"
+        });
+    }
 });
-
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 app.post('/update-cobj', async (request, response) => {
    
-    const nameObject ="frutas";
     const {name, amount, quantity} = request.body;
-    const updateCustomURL = `/crm/v3/objects/${nameObject}`;
     
     const properties = {
         'name': name,
         'amount': amount,
         'quantity': quantity
     };
-
-    const responseAPI = await instance.post(updateCustomURL, {properties}).then(resp => {
-        return resp.status;
-    }).catch(error => {
-        console.error(error);
-    });
+    if (request.body.flag){
+        const objectId = request.body.id
+        let updateCustomURL = `/crm/v3/objects/${OBJECT_TYPE}/${objectId}`
+        const respAPI = await instance.patch(updateCustomURL, {properties}).then(resp => {
+            console.log(JSON.stringify(resp.data));
+            return resp.data;
+        }).catch(erro => {
+            console.error(erro);
+        });
+    }
     
-    console.log("response API: ", responseAPI);
+    else{
+        const createCustomURL = `/crm/v3/objects/${OBJECT_TYPE}`;
+        const responseAPI = await instance.post(createCustomURL, {properties}).then(resp => {
+            return resp.status;
+        }).catch(error => {
+            console.error(error);
+        });
+        
+        console.log("response API: ", responseAPI);
+    }
     response.redirect('/')
 });
 
@@ -175,6 +208,23 @@ app.post('/update', async (req, res) => {
 });
 */
 
+//Recebe um array de objetos e inseri o atributo ID incremental para cada objecto
+function insertID(object){
+    let id_increment = 1;
+    object.forEach(element => {
+        element['id_row'] = id_increment;
+        id_increment++;
+    });
+    return object
+}
+
+const toListObject = (file) => {
+    let newListObject = []
+    for(const chave in file){
+        newListObject.push(file[chave]);
+    }
+    return newListObject;
+}
 
 // * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
---
+++ public/scripts/scripts.js
@@ -0,0 +1,45 @@
+const buttonElement = document.getElementsByClassName("butom-options")[0];
+const selectElement = document.getElementsByClassName("button-update-choice")[0];
+
+
+function openOptions(){
+    document.getElementById("myButton").style.display = "block";
+}
+
+function openSelectform(){
+    document.getElementById("selectObject").style.display = "block";
+    buttonElement.removeEventListener("click", openOptions, false);
+    document.getElementById("myButton").style.display = "none";
+
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    buttonElement.addEventListener("click", openOptions, false);
+
+    selectElement.addEventListener('click', openSelectform, false);
+});
+
+
+
+// function insertOptionsContent(){
+//     console.log("inside function")
+//     let object = readDataStored();
+//     const selectAtt = document.getElementById('selectID');
+//     object.forEach(obj => {
+//         console.log("object name: ", obj.name)
+//         let newElement = document.createElement('option');
+//         newElement.setAttribute("value", `${obj.name}`);
+//         selectAtt.appendChild(newElement);
+//     });
+
+// }
+
+
+// function readDataStored() {
+//     let fs = require('fs');
+//     let fileContent = fs.readFile('./stored.json', (error) => error && console.error(error));
+//     return fileContent;
+// }
+
+// insertOptionsContent();
+
